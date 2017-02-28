2dec81fbdb49e853565729328fec1d5e4bef12c2
JoshAnnis <Josh_Annis@yahoo.com>
2017-02-28 07:52:18 -0600

    added more info to tables

---
+++ TrashPickup/TrashPickup/Models/Member.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TrashPickup.Models
+{
+    public class Member
+    {
+        [Key]
+        public int id { get; set; }
+        public string name { get; set; }
+        public string address { get; set; }
+        public string  state { get; set; }
+        public string city { get; set; }
+        public string zip { get; set; }
+
+
+
+    }
+}
---
+++ TrashPickup/TrashPickup/Models/Worker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.Web;
+
+namespace TrashPickup.Models
+{
+    public class Worker
+    {
+        [Key]
+        public int id { get; set; }
+        public string name { get; set; }
+
+    }
+}
